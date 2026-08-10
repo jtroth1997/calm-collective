eb6c505b41230ce88520422e0e3d0c75827344ab
jtroth1997 <troth.jack@yahoo.co.uk>
2026-08-10 17:44:51 +0100

    Activate Google Calendar booking endpoint

--- booking-config.js
+++ booking-config.js
@@ -1,1 +1,1 @@
-window.CALM_BOOKING_API_URL = '';
+window.CALM_BOOKING_API_URL = 'https://script.google.com/macros/s/AKfycbzPWtoe2viMF-unN5esIG4Il9t_sTzTmEe8kFxp2ReQgkCoVr0N2j6bO1iMOEj9TAWrkw/exec';

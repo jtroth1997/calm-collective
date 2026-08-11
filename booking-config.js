a1d4ccc8838e96a459ef41353798eb2468d4f98c
jtroth1997 <troth.jack@yahoo.co.uk>
2026-08-11 10:39:55 +0100

    Connect the new enquiry web app deployment

--- booking-config.js
+++ booking-config.js
@@ -1,1 +1,1 @@
-window.CALM_BOOKING_API_URL = 'https://script.google.com/macros/s/AKfycbzPWtoe2viMF-unN5esIG4Il9t_sTzTmEe8kFxp2ReQgkCoVr0N2j6bO1iMOEj9TAWrkw/exec';
+window.CALM_BOOKING_API_URL = 'https://script.google.com/macros/s/AKfycbx-0Gj8U1aRyjcrvSOgKeRAdJ6nsAEznN9ZTNf--2inzv5LskQxp4xaWH4ZMO5F6cPGvg/exec';
